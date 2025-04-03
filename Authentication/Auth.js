6f204624c7e31fb54d5591422d725532d52c2984
pratikacharya1234 <acharyapratik214@gmail.com>
2025-04-03 10:09:59 -0600

    add authentication script and link to login page

---
+++ Authentication/Auth.js
@@ -0,0 +1,12 @@
+const fname = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+if (fname === "" || email == "" || password == ""){
+    alert("Please filled the form")
+}
+else{
+    alert("Login Sucessufll");
+}
+
+
